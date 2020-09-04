a79834885bbdf81e996bdd50511708b220db997d
loghmanb <loghmanb@gmail.com>
2020-09-04 10:36:09 +0430

    [utils] add a validator to check size limit of uploaded image

---
+++ pages/utils.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    KiddosDjango,
+#    Copyright (C) 2020 Loghman Barari (<https://github.com/loghmanb/KiddosDjango>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from django.core.exceptions import ValidationError
+
+
+# set image limit to 200 KB
+IMAGE_LIMIT = 200 * 1024
+
+
+def validate_image_size(fieldfile_obj):
+    filesize = fieldfile_obj.file.size
+    if filesize > IMAGE_LIMIT:
+        raise ValidationError("Max file size is %s Byes" % str(IMAGE_LIMIT))
