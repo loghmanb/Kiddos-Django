280cf982be55573ffbafd23c884ffe403e40db35
Loghman Barari <barari@psg.net>
2020-11-17 15:44:26 +0330

    revise project setting

--- kiddos/settings.py
+++ kiddos/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
+SECRET_KEY = os.environ['SECRET_KEY']
+print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG', True))
 
